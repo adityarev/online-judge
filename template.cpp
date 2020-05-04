a42e0c8c1a4e4c53d03fa1e4aa89cab0e330ac02
adityarev <aditya.vritalia@gmail.com>
2020-05-05 00:12:22 +0700

    Add assert lib and change template structure

--- template.cpp
+++ template.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cassert>
 #include <climits>
 #include <cmath>
 #include <cstdio>
@@ -22,74 +23,10 @@
 typedef long long LL;
 
 struct FastIO {
-	template <typename T> T
-	getnumb() {
-		T c = gc();
-		
-		std::function<bool()> is_negative = [&]() -> bool {
-			for (; c < '0' || c > '9'; c = gc()) {
-				if (c == '-') {
-					c = gc();
-					if (c >= '0' && c <= '9')
-						return true;
-				}
-			}
-			return false;
-		};
-		
-		bool negative = is_negative();
-		T val = 0;
-		
-		for (; c >= '0' && c <= '9'; c = gc())
-			val = (val << 1) + (val << 3) + c - (T)'0';
-		
-		return negative ? -val : val;
-	}
-	
-	std::string
-	getstr(bool line_scan = false) {
-		std::function<bool(char&)> is_separator = [&](char &c) -> bool {
-			return c == '\n' || (c == ' ' && !line_scan) || c == EOF;
-		};
-		
-		char c;
-		std::string str = "";
-		
-		while ((c = gc()) && is_separator(c));
-		do {
-			str += c;
-		} while ((c = gc()) && !is_separator(c));
-		
-		return str;
-	}
-	
-	template <typename T> void
-	printnumb(T val, std::string suffix = "") {
-		std::function<void(T)> _print = [&](T x) -> void {
-			if (x == 0)
-				return;
-			
-			_print(x / 10);
-			pc('0' + (x % 10));
-		};
-		
-		if (val != 0) {
-			if (val < 0) {
-				pc('-');
-				val = -val;
-			}
-			_print(val);
-		} else {
-			pc('0');
-		}
-		printstr(suffix);
-	}
-	
-	void
-	printstr(std::string &str) {
-		for (char &c: str)
-			pc(c);
-	}
+	template <typename T> T getnumb();
+	template <typename T> void printnumb(T val, std::string suffix = "");
+	std::string getstr(bool line_scan = false);
+	void printstr(std::string &str);
 } fio;
 
 int
@@ -97,5 +34,77 @@ main() {
 	// Your code goes here
 	
 	return 0;
+}
+
+/* FastIO Implementation */
+template <typename T>
+T
+FastIO::getnumb() {
+	T c = gc();
+	
+	std::function<bool()> is_negative = [&]() -> bool {
+		for (; c < '0' || c > '9'; c = gc()) {
+			if (c == '-') {
+				c = gc();
+				if (c >= '0' && c <= '9')
+					return true;
+			}
+		}
+		return false;
+	};
+	
+	bool negative = is_negative();
+	T val = 0;
+	
+	for (; c >= '0' && c <= '9'; c = gc())
+		val = (val << 1) + (val << 3) + c - (T)'0';
+	
+	return negative ? -val : val;
+}
+
+template <typename T>
+void
+FastIO::printnumb(T val, std::string suffix) {
+	std::function<void(T)> _print = [&](T x) -> void {
+		if (x == 0)
+			return;
+		
+		_print(x / 10);
+		pc('0' + (x % 10));
+	};
+	
+	if (val != 0) {
+		if (val < 0) {
+			pc('-');
+			val = -val;
+		}
+		_print(val);
+	} else {
+		pc('0');
+	}
+	printstr(suffix);
+}
+
+std::string
+FastIO::getstr(bool line_scan) {
+	std::function<bool(char&)> is_separator = [&](char &c) -> bool {
+		return c == '\n' || (c == ' ' && !line_scan) || c == EOF;
+	};
+	
+	char c;
+	std::string str = "";
+	
+	while ((c = gc()) && is_separator(c));
+	do
+		str += c;
+	while ((c = gc()) && !is_separator(c));
+	
+	return str;
+}
+
+void
+FastIO::printstr(std::string &str) {
+	for (char &c: str)
+		pc(c);
 }
 
